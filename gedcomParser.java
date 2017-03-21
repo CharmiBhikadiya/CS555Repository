5b3658ea70c505603e0f37d265917dbc3800fd0c
rsutariy <ruchikasutariya@gmail.com>
2017-03-20 21:48:14 -0400

    Update gedcomParser.java

--- gedcomParser.java
+++ gedcomParser.java
@@ -450,68 +450,72 @@ public class gedcomParser {
 		return errors;
 	}
 	
-	
 	//Sprint 2 - Ruchika Sutariya -US 17 - Parents should not marry any of their descendants
-public static void ChkNoMarriageDescendent(ArrayList<indi> indArray, ArrayList<fam> famArray){
-	ArrayList<String> errors = new ArrayList<String>();
-	for(int i=0;i<famArray.size();i++)
-	{ 
-				
-				 String familyID=famArray.get(i).getId();
-				 
-				 for(i=0;i<indArray.size();i++)
-				 {
-					 String Child = indArray.get(i).getFamc();
-					 if(!Child.equals(null))
-					 {
-						 if(Child.equals(familyID))
-						 {
-						 
-							 String indiID = indArray.get(i).getId();
-							 System.out.println(familyID+"should not marry to descendent"+indiID);
-							 errors.add("Error US17: FAMILY:"+familyID+"should not marry to descendent INDIVIDUAL:"+ indiID);
-						 }
-					 } 
-					 
-					 else 
-					 {
-						 System.out.println("Individual has no parents");
+	public static String checkNoMarriageDescendent(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		String error = "";
+		for(int i=0;i<famArray.size();i++)
+		{ 		
+			 String familyID=famArray.get(i).getId();
+			 for(int j=0;j<indArray.size();j++)
+			 {
+				 String Child = indArray.get(j).getFamc();
+				 if(Child!=null){
+					 if(Child.equals(familyID))
+					 { 
+						 String indiID = indArray.get(j).getId();
+						 //System.out.println(familyID+"should not marry to descendent"+indiID);
+						 error = "Error US17: FAMILY:"+familyID+" Individual should not marry to descendent INDIVIDUAL:"+ indiID;
+						 return error;
 					 }
-					 
 				 }
-				
-				 
-	}
-	
-}	
-	//Sprint 2 - Ruchika Sutariya -US 18 - Siblings should not marry one another
-	public static void SiblingShouldnotMarry(ArrayList<indi> indArray, ArrayList<fam> famArray){
-		
-		for(int i=0;i<famArray.size();i++){
-			ArrayList<String> children = famArray.get(i).getChildren();
-			
-			if(famArray.get(i).getChildren()!=null)
-			{
-				String familyID=famArray.get(i).getId();
-				
-				if(children.size()>=2)
-				{
-					indi husband = getIndividualById(famArray.get(i).getHusband(), indArray);
-					indi wife = getIndividualById(famArray.get(i).getWife(),indArray);
-					String mothername=wife.getName();
-					String fathername=husband.getName();
-					
-					
-				System.out.println(children+" are Siblings who has same mother" + mothername + "and father" + fathername + "so they should not marry with each other.");
-				
-				}
-				else
-					System.out.println("There is only one child");
-				
-			}
+					 
+				  
+				  
+			 }					 
 		}
+		return error;
 	}
 
+	//Sprint 2 - Ruchika Sutariya -US 18 - Siblings should not marry one another
+	public static String checkSiblingShouldnotMarry(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		String error ="";
+		for(int i=0;i<famArray.size();i++){
+			ArrayList<String> children = famArray.get(i).getChildren();
+			if(famArray.get(i).getChildren()!=null)
+			{
+				String familyID=famArray.get(i).getId();	
+				if(children.size()>=2)
+				{
+					for(int j=0;j<children.size();j++){
+						indi currentChild = getIndividualById(children.get(j), indArray);
+						String name = currentChild.getName();
+						for(int k=0;k<famArray.size();k++){
+							String gethusband = getIndividualById(famArray.get(k).getHusband(), indArray).getName();
+							//String getwife = getIndividualById(famArray.get(k).getWife(), indArray).getName();
+							if(name.equals(gethusband)){
+								if(famArray.get(k).getWife()!=null){
+									String wife = getIndividualById(famArray.get(k).getWife(),indArray).getName();
+									for(int l=0;l<children.size();l++){
+										String currentName = getIndividualById(children.get(l), indArray).getName();
+										if(wife!=null){
+											if(wife.equals(currentName)){
+												error = "Error US18: FAMILY:"+familyID+" Sibblings shouldn't marry one another ("+famArray.get(k).getHusband()+")";
+												return error;
+											}
+										}
+									}
+								}
+								
+							}
+						}
+					}
+				}
+			}
+		}
+		return error;
+	}
+
+	
 	
 	
 	public static Date getDeathDate(String name, ArrayList<indi> indArray){
