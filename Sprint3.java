8bc45cf58d0d8d1f1c14db5057f9f6e2efff0ce1
pmparmar <priyaparmar1794@gmail.com>
2017-03-29 18:04:59 -0400

    Add files via upload
    Sprint 3 added

---
+++ Sprint3.java
@@ -0,0 +1,14 @@
+package gedcomParser;
+import gedcomParser.gedcomParser.fam;
+import gedcomParser.gedcomParser.indi;
+
+public class Sprint3 {
+	//Sprint 3 - Priya Parmar - User Story 12
+	public static void parentsNotTooOld(indi indArray, fam famArray){
+		
+	}
+	//Sprint 3 - Priya Parmar - User Story 08
+	public static void birthBeforeMarriageOfParents(indi indArray, fam famArray){
+		
+	}
+}
