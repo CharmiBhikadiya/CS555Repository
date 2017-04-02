94cfb4f71506acb6ac369fbca927d7ac8f197843
rsutariy <ruchikasutariya@gmail.com>
2017-04-02 14:51:01 -0400

    Update Sprint03.java

--- Sprint03.java
+++ Sprint03.java
@@ -10,45 +10,42 @@ import gedcomParser.gedcomParser.indi;
 
 public class Sprint3 {
 
-	//Sprint 3 - Ruchika Sutariya - User Story 34
-	public static void listLargeAgeDifferences(ArrayList<indi> indArray, ArrayList<fam> famArray){
-		ArrayList<fam> listcouple = new ArrayList<>();
+	public static String  listLargeAgeDifferences(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		String error="";
 		for(int i=0;i<famArray.size();i++)
 		{
 			fam currentfamindex=famArray.get(i);
 			String currentfamilyID=famArray.get(i).getId();
+			//System.out.println(currentfamilyID);
 			if(currentfamindex.getMarriage()!=null)
 			{
+				
 				String husband=currentfamindex.getHusband();
 				String husbandname= new gedcomParser().getName(husband, indArray);
-			//	System.out.println(husbandname);
 				int agehusband= indArray.get(i).getAge();
-			//	System.out.println(agehusband);
+				//System.out.println(agehusband);
 				String wife=currentfamindex.getWife();
-			//	System.out.println(wife);
-					for(i=0;i<indArray.size();i++)
+					for(int j=0;j<indArray.size();j++)
 					{
-						
-						String wifename= new gedcomParser().getName(wife, indArray);
-						String currentID=indArray.get(i).getId();
-						//System.out.println(currentID);
+						String wifename= new gedcomParser().getName(wife, indArray);	
+						String currentID=indArray.get(j).getId();
 						if(currentID.equals(wife))
 						{
-							int agewife=indArray.get(i).getAge();
-							
+							int agewife=indArray.get(j).getAge();
 							//System.out.println(agewife);
 							if((agewife>=(2*agehusband)) ||(agehusband>=(2*agewife)) )
-								System.out.println(currentfamilyID);
-								listcouple.add(currentfamindex);
+							{
 								
+								error = "US34: Family: "+currentfamilyID+" has largest difference, husband age is"+agehusband+" and wife age is"+agewife+"";
+								break;
+							}
 						}
+						
 					}
+					
 				}
-			
-			for( i=0;i<listcouple.size();i++){
-				System.out.println(listcouple.get(i).getId());
-			}
 		}
+		return error;
 	}
 	
 	//Sprint 3 - Ruchika Sutariya - User Story 39
