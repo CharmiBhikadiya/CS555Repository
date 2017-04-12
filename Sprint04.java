a0a920e077b83a70ffdf665bdcc1016e38458b8e
rsutariy <rsutariy@stevens.edu>
2017-04-12 18:53:21 -0400

    Create Sprint04.java
    Added User Story 29.

---
+++ Sprint04.java
@@ -0,0 +1,40 @@
+package gedcomParser;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import gedcomParser.gedcomParser.fam;
+import gedcomParser.gedcomParser.indi;
+
+public class Sprint4 {
+
+	// Sprint 4 - Ruchika Sutariya - User Story 29 : List deceased
+	public static void listdeceased(ArrayList<indi> indArray, ArrayList<fam> famArray) {
+		ArrayList<indi> finalList = new ArrayList<>();
+		for (int i = 0; i < indArray.size(); i++) {
+
+			indi currentindiindex = indArray.get(i);
+			String currentindiID = indArray.get(i).getId();
+			Date deathdate = indArray.get(i).getDeath();
+			String name = indArray.get(i).getName();
+			if (deathdate != null) {
+				finalList.add(currentindiindex);
+			}
+		}
+
+		for (int i = 0; i < finalList.size(); i++) {
+			System.out.println("List out deceased Individuals");
+			System.out.println(finalList.get(i).getId() + " - " + finalList.get(i).getName());
+		}
+	}
+
+	// Sprint 4 - Ruchika Sutariya - User Story 35: List recent births
+	public static String listrecentbirths(ArrayList<indi> indArray, ArrayList<fam> famArray) {
+		String error = "";
+
+		return error;
+	}
+
+}
