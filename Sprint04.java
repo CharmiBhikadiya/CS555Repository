7a6696d9a44418692bf59586e89227755b6c28c5
rsutariy <rsutariy@stevens.edu>
2017-04-12 20:38:25 -0400

    Added User Story 35

--- Sprint04.java
+++ Sprint04.java
@@ -23,18 +23,39 @@ public class Sprint4 {
 				finalList.add(currentindiindex);
 			}
 		}
-
+		System.out.println("List out deceased Individuals");
 		for (int i = 0; i < finalList.size(); i++) {
-			System.out.println("List out deceased Individuals");
+			
 			System.out.println(finalList.get(i).getId() + " - " + finalList.get(i).getName());
 		}
 	}
 
 	// Sprint 4 - Ruchika Sutariya - User Story 35: List recent births
-	public static String listrecentbirths(ArrayList<indi> indArray, ArrayList<fam> famArray) {
-		String error = "";
+	public static void listrecentbirths(ArrayList<indi> indArray, ArrayList<fam> famArray) {
+		ArrayList<indi> finalList = new ArrayList<>();
+		for (int i = 0; i < indArray.size(); i++) {
 
-		return error;
+			indi currentindiindex = indArray.get(i);
+			String currentindiID = indArray.get(i).getId();
+			Calendar calendar = Calendar.getInstance();
+			Date now = new Date();
+			calendar.add(Calendar.DATE, -30);
+			Date predate = calendar.getTime();
+			Date birthdate = currentindiindex.getBirth();
+			int marrmonth = birthdate.getMonth();
+			int marrdate = birthdate.getDate();
+			calendar.clear();
+			int year = Calendar.getInstance().get(Calendar.YEAR);
+			calendar.set(year, marrmonth, marrdate);
+			Date birthdate1 = calendar.getTime();
+			if ((birthdate1.after(predate)) && (birthdate1.before(now))) {
+				finalList.add(currentindiindex);
+				break;
+			}
+		}
+		System.out.println("List out Recent birthday of Individuals");
+		for (int i = 0; i < finalList.size(); i++) {
+			System.out.println(finalList.get(i).getId() + " - " + finalList.get(i).getName());
+		}
 	}
-
 }
