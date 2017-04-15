be3fcc1416ad5a1787e2e4bd1094b39d4771a8a5
pmparmar <priyaparmar1794@gmail.com>
2017-04-15 16:38:06 -0400

    Add files via upload

--- Sprint3.java
+++ Sprint3.java
@@ -1,31 +1,36 @@
 package gedcomParser;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import gedcomParser.gedcomParser.fam;
 import gedcomParser.gedcomParser.indi;
 
 public class Sprint3 {
+	static Sprint4 s4 = new Sprint4();
 	//Sprint 3 - Priya Parmar - User Story 12
 	public static String parentsNotTooOld(ArrayList<indi> indArray, ArrayList<fam> famArray){
 		String error="";
 		for(int i=0;i<famArray.size();i++){
-			fam currentFam = famArray.get(i);
-			indi mother = new gedcomParser().getIndividualById(currentFam.getWife(), indArray);
-			indi father = new gedcomParser().getIndividualById(currentFam.getHusband(), indArray);
-			int motherAge = mother.getAge();
-			int fatherAge = father.getAge();
-			ArrayList<String> children = currentFam.getChildren();
-			for(int j=0;j<children.size();j++){
-				indi child = new gedcomParser().getIndividualById(children.get(j), indArray);
-				int childAge = child.getAge();
-				if(motherAge-childAge>60){
-					error = "US12: INDIVIDUAL: "+mother.getId()+" Mother is too old";
-					break;
-				}else if(fatherAge-childAge>80){
-					error = "US12: INDIVIDUAL: "+father.getId()+" Father is too old";
-					break;
+			if(famArray.get(i).getMarriage() != null){
+				fam currentFam = famArray.get(i);
+				indi mother = new gedcomParser().getIndividualById(currentFam.getWife(), indArray);
+				indi father = new gedcomParser().getIndividualById(currentFam.getHusband(), indArray);
+				int motherAge = mother.getAge();
+				int fatherAge = father.getAge();
+				ArrayList<String> children = currentFam.getChildren();
+				for(int j=0;j<children.size();j++){
+					indi child = new gedcomParser().getIndividualById(children.get(j), indArray);
+					int childAge = child.getAge();
+					if(motherAge-childAge>60){
+						error = "Error US12: INDIVIDUAL: "+mother.getId()+" Mother is too old: Line: "+s4.getLineNumber();
+						break;
+					}else if(fatherAge-childAge>80){
+						error = "Error US12: INDIVIDUAL: "+father.getId()+" Father is too old: Line: "+s4.getLineNumber();
+						break;
+					}
 				}
 			}
 		}
@@ -42,13 +47,155 @@ public class Sprint3 {
 				for(int j=0;j<children.size();j++){
 					indi child = new gedcomParser().getIndividualById(children.get(j), indArray);
 					Date birthDate = child.getBirth();
-					if(birthDate.before(mrgDate)){
-						error = "US08: INDIVIDUAL: "+child.getId()+" has birth date before marriage of parents";
-						break;
-					}
+					if(birthDate!= null){
+						if(birthDate.before(mrgDate)){
+							error = "Error US08: INDIVIDUAL: "+child.getId()+" has birth date before marriage of parents: Line: "+s4.getLineNumber();
+							break;
+						}
+					}			
 				}
 			}
 		}
 		return error;
 	}
+	
+	//Sprint 3 - Ruchika Sutariya - User Story 34
+	public static String  listLargeAgeDifferences(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		String error="";
+		for(int i=0;i<famArray.size();i++)
+		{
+			fam currentfamindex=famArray.get(i);
+			String currentfamilyID=famArray.get(i).getId();
+			//System.out.println(currentfamilyID);
+			if(currentfamindex.getMarriage()!=null)
+			{
+				
+				String husband=currentfamindex.getHusband();
+				String husbandname= new gedcomParser().getName(husband, indArray);
+				int agehusband= indArray.get(i).getAge();
+				//System.out.println(agehusband);
+				String wife=currentfamindex.getWife();
+					for(int j=0;j<indArray.size();j++)
+					{
+						String wifename= new gedcomParser().getName(wife, indArray);	
+						String currentID=indArray.get(j).getId();
+						if(currentID.equals(wife))
+						{
+							int agewife=indArray.get(j).getAge();
+							//System.out.println(agewife);
+							if((agewife>=(2*agehusband)) ||(agehusband>=(2*agewife)) )
+							{
+								
+								error = "US34: Family: "+currentfamilyID+" has largest difference, husband age is "+agehusband+" and wife age is "+agewife+"";
+								break;
+							}
+						}
+						
+					}
+					
+				}
+		}
+		return error;
+	}
+	
+	//Sprint 3 - Ruchika Sutariya - User Story 39
+	public String listUpcomingAnniversaries(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		String error="";
+		for(int i=0;i<famArray.size();i++)
+		{
+			fam currentfamindex=famArray.get(i);
+			String currentfamilyID=famArray.get(i).getId();
+			if(currentfamindex.getMarriage()!=null)
+			{
+				
+					String husband=currentfamindex.getHusband();
+					String wife =currentfamindex.getWife();
+					Date deathhusband=indArray.get(i).getDeath();
+					if(deathhusband==null)
+						for(int j=0;j<indArray.size();j++)
+						{
+							String wifeid=indArray.get(j).getId();
+							if(wifeid.equals(wife))
+							{
+								Date deathwife=indArray.get(j).getDeath();
+									if(deathwife==null)
+									{		
+										
+										Calendar calendar = Calendar.getInstance();
+										Date now = new Date();
+										//System.out.println(new SimpleDateFormat("MM-dd-yyyy").format(now));
+										calendar.add(Calendar.DATE, 30);
+										Date nextdate = calendar.getTime();
+										//System.out.println(new SimpleDateFormat("MM-dd-yyyy").format(nextdate));
+										Date marriagedate=currentfamindex.getMarriage();
+										//System.out.println(marriagedate);
+										int marrmonth=marriagedate.getMonth();
+										int marrdate=marriagedate.getDate();	
+										calendar.clear();
+										int year = Calendar.getInstance().get(Calendar.YEAR);
+										calendar.set(year, marrmonth, marrdate);
+										Date marriagedate1=calendar.getTime();	
+										if((marriagedate1.after(now)) &&(marriagedate1.before(nextdate)))
+										{
+											//System.out.println(currentfamilyID);
+											error = "US39: FAMILY:"+currentfamilyID+" has upcoming marriage anniversary(In Next 30 Days).";
+											break;
+										}
+							
+									}
+							}
+						}
+			}		
+		}
+		return error;
+	}
+	
+	//Sprint 3 - Charmi Bhikadiya - User Story 30
+	public static void listLivingMarried(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		System.out.println("US30: List of all people who are living and married:");
+		ArrayList<indi> finalList = new ArrayList<>();
+		for(int i=0;i<indArray.size();i++){
+			indi currentIndi = indArray.get(i);
+			if(!currentIndi.getFams().isEmpty()){
+				if(currentIndi.getDeath() == null){
+					finalList.add(currentIndi);
+				}
+			}
+		}
+		for(int i=0;i<finalList.size();i++){
+			System.out.println(finalList.get(i).getName());
+		}
+	}
+	
+	//Sprint 3 - Charmi Bhikadiya - User Story 33
+	public static String listOrphans(ArrayList<indi> indArray, ArrayList<fam> famArray){
+		String note = ""; 
+		for(int k=0;k<famArray.size();k++){
+			fam currentfam = famArray.get(k);
+			String husband = currentfam.getHusband(); 
+			String wife = currentfam.getWife(); 
+			ArrayList<Date> chk = new gedcomParser(). getBirthDeath(husband, indArray);
+			ArrayList<Date> chk1 = new gedcomParser(). getBirthDeath(wife, indArray);
+			Date hdeath = new gedcomParser(). getBirthDeath(husband, indArray).get(1);
+			Date wdeath = new gedcomParser().getBirthDeath(wife, indArray).get(1);
+			
+			if(hdeath != null && wdeath != null){
+				ArrayList<String> children = currentfam.getChildren();
+				for(int i = 0; i < children.size(); i++){
+					for(int j = 0; j < indArray.size(); j++){
+						if(indArray.get(j).id.equals(children.get(i))){
+							int age = indArray.get(j).age;
+							if (age < 18){
+								note = "US33: Child " + children.get(i) + " in family " + currentfam.id + " is orphaned.\n";
+								break;
+							}
+						}
+					}
+				}
+				
+			}
+		}
+		return note;
+	}
+	
 }
--- Sprint4.java
+++ Sprint4.java
@@ -3,33 +3,37 @@ package gedcomParser;
 import gedcomParser.gedcomParser.fam;
 import gedcomParser.gedcomParser.indi;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Sprint4 {
+	//Sprint 4 - Priya Parmar - User Story 22: Unique IDs
 	public static void checkUniqueIndiId(indi i, ArrayList<indi> individuals) {
         String indId = i.getId();
         
         for (int j = 0; j < individuals.size(); j++) {
             String id = individuals.get(j).getId();
             if (id.equals(indId) != individuals.get(j).equals(j)) {
-                System.out.println("Error US22: Check Unique ID: Individual "+ i.getName().replace("/", "") +" (" +id +") " +"has duplicate ID");
+                System.out.println("Error US22: Check Unique ID: Individual "+ i.getName().replace("/", "") +" (" +id +") " +"has duplicate ID: Line: "+getLineNumber());
+                break;
             }
-        }//System.out.println("every Individual IDs are Unique!!"); 
-        
-        
+        }
     }
 	public static void checkUniqueFamId(fam f, ArrayList<fam> families) {
 		String famId = f.getId();
 		for (int j = 0; j < families.size(); j++) {
             String id = families.get(j).getId();
             if (families.get(j).equals(f) != id.equals(famId)) {
-                System.out.println("Error US22: Check Unique ID: Family with Husband "+  f.getHusband() + "and Wife  " + f.getWife() +" (" +id +") " +"has duplicate ID");
+                System.out.println("Error US22: Check Unique ID: Family with Husband "+  f.getHusband() + "and Wife  " + f.getWife() +" (" +id +") " +"has duplicate ID: Line: "+getLineNumber());
+                break;
             } 
-        }//System.out.println("every Family IDs are Unique!!");
+        }
 	}
+	//Sprint 4 - Priya Parmar - User Story 25: Unique first names in families
 	public static String checkUniqueChildren(ArrayList<indi> indArray, fam currentfam){
-			
+		int flag=0;
 		ArrayList<String> children = currentfam.getChildren();
 		ArrayList<String> childrenNames = new ArrayList<String>();
 		ArrayList<Date> childrenBdays = new ArrayList<Date>();
@@ -52,11 +56,15 @@ public class Sprint4 {
 				int index2 = duplicateNames.get(i+1);
 				if (childrenBdays.get(index1).equals(childrenBdays.get(index2))){
 					error = "Error US25: FAMILY: " + currentfam.getId() + " There are multiple children with same name and birthdate in family: Line: "+getLineNumber();
+					flag=1;
 					break;
 				}
 				i +=2; 
 			}
+		}else{
+			error = "US25: All families have unique first names";
 		}
+		
 		return error;
 	}
 	public static ArrayList<Integer> distinctValues(ArrayList<String> arr){
@@ -71,8 +79,75 @@ public class Sprint4 {
 	    }              
 	    return duplicateIndexes;         
 	}
-	
+	// Sprint 4 - Ruchika Sutariya - User Story 29 : List deceased
+	public static void listdeceased(ArrayList<indi> indArray, ArrayList<fam> famArray) {
+		ArrayList<indi> finalList = new ArrayList<>();
+		for (int i = 0; i < indArray.size(); i++) {
+
+			indi currentindiindex = indArray.get(i);
+			String currentindiID = indArray.get(i).getId();
+			Date deathdate = indArray.get(i).getDeath();
+			String name = indArray.get(i).getName();
+			if (deathdate != null) {
+				finalList.add(currentindiindex);
+			}
+		}
+		System.out.println("US 29: List out deceased Individuals");
+		for (int i = 0; i < finalList.size(); i++) {
+			
+			System.out.println(finalList.get(i).getId() + " - " + finalList.get(i).getName());
+		}
+	}
+	// Sprint 4 - Ruchika Sutariya - User Story 35: List recent births
+	public static void listrecentbirths(ArrayList<indi> indArray, ArrayList<fam> famArray) {
+		ArrayList<indi> finalList = new ArrayList<>();
+		for (int i = 0; i < indArray.size(); i++) {
+
+			indi currentindiindex = indArray.get(i);
+			String currentindiID = indArray.get(i).getId();
+			Calendar calendar = Calendar.getInstance();
+			Date now = new Date();
+			calendar.add(Calendar.DATE, -30);
+			Date predate = calendar.getTime();
+			Date birthdate = currentindiindex.getBirth();
+			int marrmonth = birthdate.getMonth();
+			int marrdate = birthdate.getDate();
+			calendar.clear();
+			int year = Calendar.getInstance().get(Calendar.YEAR);
+			calendar.set(year, marrmonth, marrdate);
+			Date birthdate1 = calendar.getTime();
+			if ((birthdate1.after(predate)) && (birthdate1.before(now))) {
+				finalList.add(currentindiindex);
+				break;
+			}
+		}
+		System.out.println("US 35: List out Recent birthday of Individuals");
+		for (int i = 0; i < finalList.size(); i++) {
+			System.out.println(finalList.get(i).getId() + " - " + finalList.get(i).getName());
+		}
+	}
+	//Sprint 4 - Charmi Bhikadiya - User Story 40: Include input line numbers
 	public static int getLineNumber() {
 	    return Thread.currentThread().getStackTrace()[2].getLineNumber();
 	}
+	//Sprint 4 - Charmi Bhikadiya - User Story 27: Include individual ages
+	public static int getIndiAge(String sDate) {
+        SimpleDateFormat dt = new SimpleDateFormat("dd MMM yyyy"); 
+	    Date date; 
+	    try{
+            date = dt.parse(sDate);
+            //System.out.println(date);
+            Date currentDate = new Date();
+            //if (deathcheck=false) {
+            long difference = currentDate.getTime() - date.getTime();
+            long Hours = difference / (60 * 60 * 1000);
+            long Days = Hours / 24; 
+            double Year = Days / 365;
+            int year = (int) Year;
+            //  System.out.println(year);
+            return year;
+         }
+         catch (Exception ex) {}
+        return 0;
+    }
 }
